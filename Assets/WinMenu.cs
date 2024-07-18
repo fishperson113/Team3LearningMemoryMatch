29a31ac416ee7aca04e3c8fd43b4d502f9095088
Ama2352 <honguyenminhsang2016@gmail.com>
2024-07-18 12:22:21 +0700

    WinAnnouncementUpdate3(Bug ReplayButton)

--- Assets/WinMenu.cs
+++ Assets/WinMenu.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class WinMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {    
-        SceneManager.LoadSceneAsync(1);
-    }    
-    public void QuitGame()
-    {
-        Debug.Log("Quit!");
-        Application.Quit();
-    } 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
     }    
-
 }
